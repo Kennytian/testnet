933a0f70661e298f628b5b33e99cdae9d9d3c5c0
Kennytian <kt.tian@gmail.com>
2022-06-10 16:36:38 +0800

    feat: Add some sol files

--- contracts/GettersSetters.sol
+++ contracts/GettersSetters.sol
@@ -4,12 +4,22 @@ pragma solidity >=0.5.0 <0.9.0;
 contract GettersSetters {
     int public price;
     string public location = 'China';
+    address immutable public owner;
+    int constant area = 100;
+    int immutable age = 30;
+
+    // Set values when the deploy
+    constructor(int _price, string memory _location) {
+        price = _price;
+        location = _location;
+        owner = msg.sender;
+    }
 
     function setPrice(int _price) public {
         price = _price;
     }
 
-    function getPrice() public view returns(int) {
+    function getPrice() public view returns (int) {
         return price;
     }
 
