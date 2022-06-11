300d6666680c24d69ec97f48335031005d088dfa
Kennytian <kt.tian@gmail.com>
2022-06-11 14:09:09 +0800

    feat: Add some sol files

---
+++ contracts/DynamicArrays.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract DynamicArrays {
+    uint [] public numbers;
+
+    function getLength() public view returns (uint) {
+        return numbers.length;
+    }
+
+    function addElement(uint item) public {
+        numbers.push(item);
+    }
+
+    function getElement(uint index) public view returns (uint) {
+        require(index < numbers.length, 'index >= numbers.length');
+        return numbers[index];
+    }
+
+    function popElement() public {
+        numbers.pop();
+    }
+
+    function f() {
+        uint[] memory arr = new uint[](3);
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        numbers = arr;
+    }
+}
